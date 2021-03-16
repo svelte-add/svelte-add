a2c1e76dd12e9890a10d2f5444fdb06de98d647d
Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-03-16 13:56:32 -0400

    🐛 fix: remove console logs because the pnpx problem lies elsewhere

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 import { createRequire } from 'module';
-console.log("before createRequire");
 const require = createRequire(import.meta.url);
-console.log("after createRequire");
 
 let [node, index, adder, ...args] = process.argv;
 
@@ -12,6 +10,4 @@ if (!args.includes("--no-ssh")) args = [...args, "--no-ssh"];
 
 process.argv = [node, index, adder, ...args];
 
-console.log("before requiring apply");
 require("apply/bin/run");
-console.log("after requiring apply");
