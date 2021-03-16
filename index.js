059b9d073a8f1fd615e36fee4f702be7393d9e57
Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-03-15 21:07:53 -0400

    ⚡️ feat: esm + type checking

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 let [node, index, adder, ...args] = process.argv;
 
 if (!adder.includes("/")) adder = `svelte-add/${adder}`;
