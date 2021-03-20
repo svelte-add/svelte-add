627a3493dd61cc4df0c4f1c2ea00d57dd029d7a2
Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-03-20 01:29:46 -0400

    ⚡️ feat+fix: show an error when an adder isn't specified and try to fix Windows `pnpx` bug and support Node 12

---
+++ global.d.ts
@@ -0,0 +1,1 @@
+declare module "apply/bin/run";
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+const { logger } = require("@poppinss/cliui");
 
 let [node, index, adder, ...args] = process.argv;
+
+if (!adder) {
+    logger.error(`No adder was specified. Read ${logger.colors.cyan("https://github.com/svelte-add/svelte-adders")} to see available adders and usage.`);
+    process.exit(0);
+}
 
 if (!adder.includes("/")) adder = `svelte-add/${adder}`;
 if (!args.includes("--no-ssh")) args = [...args, "--no-ssh"];
