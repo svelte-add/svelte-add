6d06c17111448b6e4036361322094b9b3d122629
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-05-23 11:39:09 -0400

    🧹 chore: simplify + fix Rollup configs

--- projects/create-vite/rollup.config.js
+++ projects/create-vite/rollup.config.js
@@ -1,8 +1,7 @@
 import { builtinModules } from "module";
-import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 import pkg from "./package.json";
 
-/** @type{import("rollup").RollupOptions} */
+/** @type {import("rollup").RollupOptions} */
 const config = {
     external: [...builtinModules, ...Object.keys(pkg.dependencies ?? {})],
     input: "cli.js",
@@ -10,12 +9,7 @@ const config = {
         banner: "#!/usr/bin/env node",
         file: "cli.cjs",
         format: "cjs",
-        inlineDynamicImports: true,
     },
-    plugins: [
-        dynamicImportVars(),
-    ]
 };
 
-/** @type{import("rollup").RollupOptions} */
 export default config;
