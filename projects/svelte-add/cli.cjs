157a758533d104fc09db8056b9eb72955175dec1
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-06-15 15:46:34 -0400

    ⚡️ feat: warn when on Node 12 with link to https://github.com/svelte-add/svelte-add/issues/41

--- projects/svelte-add/cli.cjs
+++ projects/svelte-add/cli.cjs
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-import("./cli.js");
+import("./cli.js").catch((error) => {
+	const { bold, red } = require("kleur");
+	if (error.name === "SyntaxError") console.error(bold(red(`You must be on the latest version of Node 14 or 16 to use svelte-add. You're currently on ${process.version}\nSee the existing issue at https://github.com/svelte-add/svelte-add/issues/41`)));
+	throw error;
+});
