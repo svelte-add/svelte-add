2aeaef4b93bf812e412a2ace5cb6bccbf8e7181f
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-06-15 13:53:10 -0400

    ⚡️ feat: test improvements (more closely mirror the CLI)

--- projects/test/tests/one-adder.js
+++ projects/test/tests/one-adder.js
@@ -1,9 +1,9 @@
-import { readdir, rm } from "fs/promises";
+import { rm } from "fs/promises";
 import { inspect } from "util";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
 
-import { detectAdder, getEnvironment, runAdder } from "svelte-add";
+import { adderDependencies, detectAdder, getEnvironment, runAdder } from "svelte-add";
 import { fresh as svelteKit } from "@svelte-add/create-kit/__init.js";
 import { fresh as vite } from "@svelte-add/create-vite/__init.js";
 
@@ -11,9 +11,23 @@ const initializers = { svelteKit, vite };
 const adders = await readdir("node_modules/svelte-add/adders");
 
 for (const [app, init] of Object.entries(initializers)) {
-	for (const adder of adders) {
-		test(`${adder} being used on ${app} (without demos)`, async () => {
-			const output = `_outputs/${app}_${adder}`;
+	for (const adderToTest of addersToTest) {
+
+		/** @type {string[]} */
+		const addersToCheck = [];
+		const dependencies = adderDependencies[adderToTest];
+
+		for (const dependency of dependencies) {
+			// Move dependencies to the front such that
+			// tailwindcss+postcss is rewritten as postcss+tailwindcss
+			if (addersToCheck.includes(dependency)) addersToCheck.splice(addersToCheck.indexOf(dependency), 1);
+
+			addersToCheck.unshift(dependency);
+		}
+		if (!addersToCheck.includes(adderToTest)) addersToCheck.push(adderToTest);
+		
+		test(`${adderToTest} being used on ${app} (without demos)`, async () => {
+			const output = `_outputs/${app}_${adderToTest}`;
 			await rm(output, {
 				recursive: true,
 				force: true,
@@ -30,32 +44,38 @@ for (const [app, init] of Object.entries(initializers)) {
 
 			let environment = await getEnvironment({ cwd: output });
 
-			const preRunCheck = await detectAdder({
-				adder,
-				cwd: output,
-				environment,
-			});
+			/** @type {string[]} */
+			const addersToRun = [];
 
-			assert.ok(Object.values(preRunCheck).every((pass) => !pass), `Somehow, pre-run checks show that the integration is already set up: ${inspect(preRunCheck)}`);
-			
-			await runAdder({
-				adder,
-				cwd: output,
-				environment,
-				// TODO: fix so tests can be run on Windows
-				npx: "pnpx",
-				options: {},
-			});
+			for (const adderToCheck of addersToCheck) {
+				const preRunCheck = await detectAdder({
+					adder: adderToCheck,
+					cwd: output,
+					environment,
+				});
+				assert.ok(Object.values(preRunCheck).every((pass) => !pass), `Somehow, pre-run checks show that ${adderToTest} is already set up: ${inspect(preRunCheck)}`);
+				addersToRun.push(adderToCheck);
+			}
 
-			environment = await getEnvironment({ cwd: output });
+			for (const adderToRun of addersToRun) {
+				await runAdder({
+					adder: adderToRun,
+					cwd: output,
+					environment,
+					npx: "pnpx",
+					options: { jit: true },
+				});
+				environment = await getEnvironment({ cwd: output });
+			}
 
-			const postRunCheck = await detectAdder({
-				adder,
-				cwd: output,
-				environment,
-			});
-
-			assert.ok(Object.values(postRunCheck).every(Boolean), `The integration was not set up correctly: ${inspect(postRunCheck)}`);
+			for (const adderToCheck of addersToCheck) {
+				const postRunCheck = await detectAdder({
+					adder: adderToCheck,
+					cwd: output,
+					environment,
+				});
+				assert.ok(Object.values(postRunCheck).every(Boolean), `${adderToTest} was not set up correctly: ${inspect(postRunCheck)}`);
+			}
 		})
 	}
 }
