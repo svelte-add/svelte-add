3a71a78c09004373002c036a4eb1c541d6f590ff
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-06-20 19:33:02 -0400

    🐛 fix: remove placeholder stuff in `create-vite` that doesn't work (will put it back when ready)

--- projects/create-vite/cli.js
+++ projects/create-vite/cli.js
@@ -1,15 +1,5 @@
-import { getChoices } from "svelte-add";
-
 const main = async () => {
-    console.error("creating a vite-powered svelte app like this doesn't work yet");
-
-    const choices = await getChoices({
-        args: process.argv.slice(2),
-        promptMissing: true,
-    });
-
-    // TODO: make proper use of choices
-    console.log({ choices });
+	console.error("creating a vite-powered svelte app like this doesn't work yet");
 };
 
 main();
