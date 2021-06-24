82370e8d65c5deb5d1edd4a0e0a95b2ed0eeac95
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-06-24 13:43:41 -0400

    🧹 chore: refactor tools for updating Svelte config so it's easier not to duplicate them across adders

--- projects/svelte-add/ast-tools.js
+++ projects/svelte-add/ast-tools.js
@@ -137,3 +137,229 @@ export const getConfigObject = ({ cjs, typeScriptEstree }) => {
 
 	return configObjectExpression;
 };
+
+/**
+ *
+ * @param {object} param0
+ * @param {boolean} param0.cjs
+ * @param {string} param0.package
+ * @param {import("./ast-io.js").RecastAST} param0.typeScriptEstree
+ * @returns {string | undefined}
+ */
+export const findImport = ({ cjs, package: pkg, typeScriptEstree }) => {
+	/** @type {string | undefined} */
+	let packageImportedAs;
+
+	walk(typeScriptEstree, {
+		enter(node) {
+			if (cjs) {
+				if (node.type !== "VariableDeclarator") return;
+
+				/** @type {import("estree").VariableDeclarator} */
+				// prettier-ignore
+				const declarator = (node)
+
+				if (declarator.id.type !== "Identifier") return;
+				const identifier = declarator.id;
+
+				if (!declarator.init) return;
+				if (declarator.init.type !== "CallExpression") return;
+				const callExpression = declarator.init;
+
+				if (callExpression.callee.type !== "Identifier") return;
+				const callee = callExpression.callee;
+
+				if (callee.name !== "require") return;
+
+				if (callExpression.arguments[0].type !== "Literal") return;
+				if (callExpression.arguments[0].value !== pkg) return;
+
+				packageImportedAs = identifier.name;
+			} else {
+				if (node.type !== "ImportDeclaration") return;
+
+				/** @type {import("estree").ImportDeclaration} */
+				// prettier-ignore
+				const importDeclaration = (node)
+
+				if (importDeclaration.source.value !== pkg) return;
+
+				for (const specifier of importDeclaration.specifiers) {
+					if (specifier.type === "ImportDefaultSpecifier") packageImportedAs = specifier.local.name;
+				}
+			}
+		},
+	});
+
+	return packageImportedAs;
+};
+
+/**
+ *
+ * @param {object} param0
+ * @param {string} param0.all
+ * @param {boolean} param0.cjs
+ * @param {string} param0.default
+ * @param {string} param0.package
+ * @param {import("./ast-io.js").RecastAST} param0.typeScriptEstree
+ * @returns {void}
+ */
+export const addImport = ({ all, cjs, default: default_, package: pkg, typeScriptEstree }) => {
+	if (cjs) {
+		/** @type {import("estree").VariableDeclaration} */
+		const requirePackageAst = {
+			type: "VariableDeclaration",
+			declarations: [
+				{
+					type: "VariableDeclarator",
+					id: {
+						type: "Identifier",
+						name: all,
+					},
+					init: {
+						type: "CallExpression",
+						// @ts-ignore - I am not sure why this is typed wrongly (?)
+						arguments: [
+							{
+								type: "Literal",
+								value: pkg,
+							},
+						],
+						callee: {
+							type: "Identifier",
+							name: "require",
+						},
+						optional: false,
+					},
+				},
+			],
+			kind: "const",
+		};
+
+		typeScriptEstree.program.body.unshift(requirePackageAst);
+	} else {
+		/** @type {import("estree").ImportDeclaration} */
+		const importPackageAst = {
+			type: "ImportDeclaration",
+			source: {
+				type: "Literal",
+				value: pkg,
+			},
+			specifiers: [
+				{
+					type: "ImportDefaultSpecifier",
+					local: {
+						type: "Identifier",
+						name: default_,
+					},
+				},
+			],
+		};
+
+		typeScriptEstree.program.body.unshift(importPackageAst);
+	}
+};
+
+/**
+ *
+ * @param {object} param0
+ * @param {import("estree").ObjectExpression} param0.configObject
+ * @returns {import("estree").ArrayExpression}
+ */
+export const getPreprocessArray = ({ configObject }) => {
+	// Try to find preprocess config
+	/** @type {import("estree").Property | undefined} */
+	let preprocessConfig;
+	for (const property of configObject.properties) {
+		if (property.type !== "Property") continue;
+		if (property.key.type !== "Identifier") continue;
+		if (property.key.name !== "preprocess") continue;
+
+		preprocessConfig = property;
+	}
+	// Or set it to an empty array if it doesn't exist
+	if (!preprocessConfig) {
+		preprocessConfig = {
+			type: "Property",
+			computed: false,
+			key: {
+				type: "Identifier",
+				name: "preprocess",
+			},
+			kind: "init",
+			method: false,
+			shorthand: false,
+			value: {
+				type: "ArrayExpression",
+				elements: [],
+			},
+		};
+		configObject.properties.push(preprocessConfig);
+	}
+
+	if (preprocessConfig.value.type !== "ArrayExpression") {
+		// Convert preprocess config from a single function call to an array e.x. [svelte-preprocess()]
+		/** @type {import("estree").ArrayExpression} */
+		const preprocessArray = {
+			type: "ArrayExpression",
+			elements: [],
+		};
+		if (preprocessConfig.value.type !== "CallExpression") throw new TypeError("preprocess settings were expected to be a function call");
+		/** @type {import("estree").CallExpression} */
+		const preprocessConfigValue = preprocessConfig.value;
+		preprocessArray.elements.push(preprocessConfigValue);
+		preprocessConfig.value = preprocessArray;
+	}
+
+	return preprocessConfig.value;
+};
+
+/**
+ *
+ * @param {object} param0
+ * @param {import("estree").ArrayExpression} param0.preprocessArray
+ * @param {string} param0.sveltePreprocessImportedAs
+ * @returns {import("estree").ObjectExpression}
+ */
+export const getSveltePreprocessArgs = ({ preprocessArray, sveltePreprocessImportedAs }) => {
+	/** @type {import("estree").CallExpression | undefined} */
+	let sveltePreprocessFunctionCall;
+	for (const element of preprocessArray.elements) {
+		if (!element) continue;
+		if (element.type !== "CallExpression") continue;
+		if (element.callee.type !== "Identifier") continue;
+		if (element.callee.name !== sveltePreprocessImportedAs) continue;
+		sveltePreprocessFunctionCall = element;
+	}
+
+	// Add a svelte-preprocess() call to the config if missing
+	if (!sveltePreprocessFunctionCall) {
+		sveltePreprocessFunctionCall = {
+			type: "CallExpression",
+			// @ts-ignore - I am not sure why this is typed wrongly (?)
+			arguments: [],
+			callee: {
+				type: "Identifier",
+				name: sveltePreprocessImportedAs,
+			},
+			optional: false,
+		};
+
+		preprocessArray.elements.push(sveltePreprocessFunctionCall);
+	}
+
+	// Initialize the svelte-preprocess options as {} if none were passed
+	if (sveltePreprocessFunctionCall.arguments.length === 0) {
+		/** @type {import("estree").ObjectExpression} */
+		const emptyObject = {
+			type: "ObjectExpression",
+			properties: [],
+		};
+
+		sveltePreprocessFunctionCall.arguments.push(emptyObject);
+	}
+
+	if (sveltePreprocessFunctionCall.arguments[0].type !== "ObjectExpression") throw new TypeError("that's an unexpected argument to svelte-preprocess");
+
+	return sveltePreprocessFunctionCall.arguments[0];
+};
