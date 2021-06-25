d15078d6a6e956f272c38dc6230b6bc903b5dee4
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-06-24 20:21:07 -0400

    ⚡️ feat: mdsvex adder (`package-versions.js`)

--- projects/svelte-add/package-versions.js
+++ projects/svelte-add/package-versions.js
@@ -1,6 +1,7 @@
 export const packageVersions = {
 	autoprefixer: "^10.2.6",
 	cssnano: "^5.0.6",
+	mdsvex: "^0.9.7",
 	postcss: "^8.3.5",
 	"postcss-load-config": "^3.1.0",
 	"svelte-preprocess": "^4.7.3",
