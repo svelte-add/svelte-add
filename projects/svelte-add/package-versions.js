95c3b287ad6137f019565733b7cf7967f8ef3dbe
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-07-20 18:24:17 -0400

    🧹 chore: upgrade packages to their latest versions

--- projects/svelte-add/package-versions.js
+++ projects/svelte-add/package-versions.js
@@ -1,11 +1,11 @@
 export const packageVersions = {
-	autoprefixer: "^10.2.6",
+	autoprefixer: "^10.3.1",
 	cssnano: "^5.0.6",
-	mdsvex: "^0.9.7",
+	mdsvex: "^0.9.8",
 	postcss: "^8.3.5",
 	"postcss-load-config": "^3.1.0",
-	sass: "^1.35.1",
-	"svelte-preprocess": "^4.7.3",
+	sass: "^1.35.2",
+	"svelte-preprocess": "^4.7.4",
 	tailwindcss: "^2.2.4",
 };
 
