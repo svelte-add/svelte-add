01dcffe5e9479c61e457a3ae0e79756b4f1f32a6
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-12-21 17:43:18 -0500

    🧹 chore: remove all `cli.cjs` and only use `cli.js` (ESM) directly

--- projects/create-kit/cli.cjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import("./cli.js").catch((error) => {
-	const { bold, red } = require("kleur");
-	if (error.name === "SyntaxError") console.error(bold(red(`You must be on the latest version of Node 14 or 16 to use npm init @svelte-add/kit. You're currently on ${process.version}\nSee the existing issue at https://github.com/svelte-add/svelte-add/issues/41`)));
-	throw error;
-});
