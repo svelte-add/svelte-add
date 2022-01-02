724958f39ef4fc5afad7dd77c255af8201c375d5
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-01-01 22:58:53 -0500

    ⚡️ feat: preliminary Tauri adder (somewhat works with Vite)

--- projects/svelte-add/package-versions.js
+++ projects/svelte-add/package-versions.js
@@ -1,4 +1,5 @@
 export const packageVersions = {
+	"@sveltejs/adapter-static": "next",
 	"@tailwindcss/forms": "^0.4.0",
 	"@tailwindcss/typography": "^0.5.0",
 	autoprefixer: "^10.4.0",
@@ -6,6 +7,7 @@ export const packageVersions = {
 	bulma: "^0.9.3",
 	coffeescript: "^2.6.1",
 	mdsvex: "^0.9.8",
+	"npm-run-all": "^4.1.5",
 	sass: "^1.45.1",
 	"svelte-preprocess": "^4.10.1",
 	"svelte-cubed": "^0.2.1",
