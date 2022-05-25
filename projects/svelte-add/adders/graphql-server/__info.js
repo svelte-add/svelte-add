1b1747b7208fb40853b915baaf67fd1bacd3d1e8
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-05-24 22:30:16 -0400

    🎨 style: make generated READMEs not need to be formatted because they'll match what that looks like from the beginning

--- projects/svelte-add/adders/graphql-server/__info.js
+++ projects/svelte-add/adders/graphql-server/__info.js
@@ -2,7 +2,7 @@ export const name = "(out of date) GraphQL server";
 
 export const emoji = "🕸";
 
-export const usageMarkdown = ["You can create a GraphQL schema in `src/graphql/schema.ts` using any library or technique you want. Additionally, you can set the `defaultQuery` that shows up when GraphiQL is loaded in the browser.", "You can query your API (with POST requests) at the /graphql endpoint.", "You can visit GraphiQL in the browser at the /graphql endpoint.", "You can see an example of how you may set up resolver-level authorization with the `contextFactory` line in `src/routes/graphql.ts` and the `authorization` argument in `src/graphql/schema.ts`.", "The `/` page (in your `src/routes/index.svelte` file) will show an example of how to use your GraphQL API in `load`.", "You [*cannot* use GraphQL subscriptions](https://github.com/svelte-add/graphql/issues/1)."];
+export const usageMarkdown = ["You can create a GraphQL schema in `src/graphql/schema.ts` using any library or technique you want. Additionally, you can set the `defaultQuery` that shows up when GraphiQL is loaded in the browser.", "You can query your API (with POST requests) at the /graphql endpoint.", "You can visit GraphiQL in the browser at the /graphql endpoint.", "You can see an example of how you may set up resolver-level authorization with the `contextFactory` line in `src/routes/graphql.ts` and the `authorization` argument in `src/graphql/schema.ts`.", "The `/` page (in your `src/routes/index.svelte` file) will show an example of how to use your GraphQL API in `load`.", "You [_cannot_ use GraphQL subscriptions](https://github.com/svelte-add/graphql/issues/1)."];
 
 /** @type {import("../..").Gatekeep} */
 export const gatekeep = async ({ folderInfo }) => {
