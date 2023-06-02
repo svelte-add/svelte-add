eb9a7e3389779aa18b1ea9a1f2b6b03ad5b0b23d
Manuel Serret <mserret99@gmail.com>
2023-06-02 15:11:59 +0200

    ⚡ feat: add initial automated testing (#35)

--- projects/test/tests/one-adder/one-adder-fresh-project.js
+++ projects/test/tests/one-adder/one-adder-fresh-project.js
@@ -1,6 +1,6 @@
 import { test } from "uvu";
 import { fresh as svelteKit } from "@svelte-add/create-kit/__init.js";
-import { fresh as vite } from "@svelte-add/create-vite/__init.js";
+// import { fresh as vite } from "@svelte-add/create-vite/__init.js";
 import { runAdderTests } from "../../run-adder-tests.js";
 
 async function executeTests() {
