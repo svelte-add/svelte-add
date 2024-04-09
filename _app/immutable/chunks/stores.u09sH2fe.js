8c67ab1f98e6e02a6b26789d8065573320ef7bba
manuel3108 <manuel3108@users.noreply.github.com>
2024-04-09 15:44:10 +0000

    deploy: f33144e1784940b8d135a02aefd7c25153c0d2de

--- _app/immutable/chunks/stores.u09sH2fe.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.D1d9MQxi.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
