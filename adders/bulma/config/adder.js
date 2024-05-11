e7aef1c5b5a7c597ee5e3a169ef2a28d47577853
CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>
2024-05-11 12:31:10 -0400

    fix: add slot element for layouts (#352)
    * add slot for layouts
    
    * changeset

--- adders/bulma/config/adder.js
+++ adders/bulma/config/adder.js
@@ -64,12 +64,14 @@ export const adder = defineAdderConfig({
             name: ({ kit }) => `${kit.routesDirectory}/+layout.svelte`,
             contentType: "svelte",
             condition: ({ kit }) => kit.installed,
-            content: ({ js, options }) => {
+            content: ({ js, options, html }) => {
                 if (options.useSass) {
                     js.imports.addEmpty(js.ast, "../app.scss");
                 } else {
                     js.imports.addEmpty(js.ast, "bulma/css/bulma.css");
                 }
+                const slot = html.element("slot");
+                html.ast.childNodes.push(slot);
             },
         },
         {
