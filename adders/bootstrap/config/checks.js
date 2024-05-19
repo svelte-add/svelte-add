b48643b1fd606c9e4d835ada2f8f12bdc8f5d134
Manuel Serret <mserret99@gmail.com>
2024-05-19 19:56:12 +0200

    remove example

--- adders/bootstrap/config/checks.js
+++ adders/bootstrap/config/checks.js
@@ -3,18 +3,4 @@ import { options } from "./options";
 
 export const checks = defineAdderChecks({
     options,
-    preconditions: [
-        {
-            name: "rust installed",
-            run: () => {
-                return { success: true, message: undefined };
-            },
-        },
-        {
-            name: "cargo installed",
-            run: () => {
-                return { success: false, message: undefined };
-            },
-        },
-    ],
 });
