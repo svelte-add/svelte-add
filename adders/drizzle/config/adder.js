903eabff344dfa6b759c69185a73bd0f73a3aba5
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-04 10:57:28 -0400

    more simplifying

--- adders/drizzle/config/adder.js
+++ adders/drizzle/config/adder.js
@@ -25,57 +25,57 @@ export const adder = defineAdderConfig({
             name: "mysql2",
             version: "^3.9.8",
             dev: false,
-            condition: ({ options }) => options.database === "mysql" && options.mysql === "mysql2",
+            condition: ({ options }) => options.mysql === "mysql2",
         },
         {
             name: "@planetscale/database",
             version: "^1.18.0",
             dev: false,
-            condition: ({ options }) => options.database === "mysql" && options.mysql === "planetscale",
+            condition: ({ options }) => options.mysql === "planetscale",
         },
         // PostgreSQL
         {
             name: "pg",
             version: "^8.11.5",
             dev: false,
-            condition: ({ options }) => options.database === "postgresql" && options.postgresql === "node-postgres",
+            condition: ({ options }) => options.postgresql === "node-postgres",
         },
         {
             name: "@types/pg",
             version: "^8.11.6",
             dev: true,
-            condition: ({ options }) => options.database === "postgresql" && options.postgresql === "node-postgres",
+            condition: ({ options }) => options.postgresql === "node-postgres",
         },
         {
             name: "@neondatabase/serverless",
             version: "^0.9.3",
             dev: false,
-            condition: ({ options }) => options.database === "postgresql" && options.postgresql === "neon",
+            condition: ({ options }) => options.postgresql === "neon",
         },
         {
             name: "postgres",
             version: "^3.4.4",
             dev: false,
-            condition: ({ options }) => options.database === "postgresql" && options.postgresql === "supabase",
+            condition: ({ options }) => options.postgresql === "supabase",
         },
         // SQLite
         {
             name: "better-sqlite3",
             version: "^10.0.0",
             dev: false,
-            condition: ({ options }) => options.database === "sqlite" && options.sqlite === "better-sqlite3",
+            condition: ({ options }) => options.sqlite === "better-sqlite3",
         },
         {
             name: "@types/better-sqlite3",
             version: "^7.6.10",
             dev: true,
-            condition: ({ options }) => options.database === "sqlite" && options.sqlite === "better-sqlite3",
+            condition: ({ options }) => options.sqlite === "better-sqlite3",
         },
         {
             name: "@libsql/client",
             version: "^0.6.1",
             dev: false,
-            condition: ({ options }) => options.database === "sqlite" && options.sqlite === "turso",
+            condition: ({ options }) => options.sqlite === "turso",
         },
     ],
     files: [
@@ -83,30 +83,29 @@ export const adder = defineAdderConfig({
             name: () => `.env`,
             contentType: "text",
             content: ({ content, options }) => {
-                if (!content.includes("DATABASE_URL=")) {
-                    if (options.docker === true) {
-                        // we'll prefill with the default docker db credentials
-                        const db = options.database === "mysql" ? "mysql" : "postgres";
-                        const port = options.database === "mysql" ? "3306" : "5432";
-                        content += `\nDATABASE_URL="${db}://root:mysecretpassword@localhost:${port}/local"`;
-                        return content;
-                    }
-                    if (options.database === "sqlite" && options.sqlite === "better-sqlite3") {
-                        content += `\nDATABASE_URL="./sqlite.db"`;
-                        return content;
-                    }
+                const DB_URL_KEY = "DATABASE_URL";
+                if (options.docker === true) {
+                    // we'll prefill with the default docker db credentials
+                    const db = options.database === "mysql" ? "mysql" : "postgres";
+                    const port = options.database === "mysql" ? "3306" : "5432";
+                    content = addEnvVar(content, DB_URL_KEY, `"${db}://root:mysecretpassword@localhost:${port}/local"`);
+                    return content;
+                }
+                if (options.sqlite === "better-sqlite3") {
+                    content = addEnvVar(content, DB_URL_KEY, `"./sqlite.db"`);
+                    return content;
+                }
 
-                    content += `\n# Replace with your DB credentials`;
-                    if (options.database === "sqlite" && options.sqlite === "turso") {
-                        content += `\nDATABASE_URL="libsql://db-name-user.turso.io"`;
-                        content += `\nDATABASE_AUTH_TOKEN=""`;
-                    }
-                    if (options.database === "mysql") {
-                        content += `\nDATABASE_URL="mysql://user:password@host:port/db-name"`;
-                    }
-                    if (options.database === "postgresql") {
-                        content += `\nDATABASE_URL="postgres://user:password@host:port/db-name"`;
-                    }
+                content += `\n# Replace with your DB credentials`;
+                if (options.sqlite === "turso") {
+                    content = addEnvVar(content, DB_URL_KEY, `"libsql://db-name-user.turso.io"`);
+                    content = addEnvVar(content, "DATABASE_AUTH_TOKEN", `""`);
+                }
+                if (options.database === "mysql") {
+                    content = addEnvVar(content, DB_URL_KEY, `"mysql://user:password@host:port/db-name"`);
+                }
+                if (options.database === "postgresql") {
+                    content = addEnvVar(content, DB_URL_KEY, `"postgres://user:password@host:port/db-name"`);
                 }
                 return content;
             },
@@ -114,8 +113,7 @@ export const adder = defineAdderConfig({
         {
             name: () => `docker-compose.yml`,
             contentType: "text",
-            condition: ({ options }) =>
-                options.database !== "sqlite" && (options.mysql === "mysql2" || options.postgresql === "node-postgres"),
+            condition: ({ options }) => options.mysql === "mysql2" || options.postgresql === "node-postgres",
             content: ({ content, options }) => {
                 // if the file already exists, don't modify it
                 // (in the future, we could add some tooling for modifying yaml)
@@ -257,13 +255,13 @@ export const adder = defineAdderConfig({
             content: ({ ast, exports, imports, options, common, functions, variables }) => {
                 let clientExpression;
                 // SQLite
-                if (options.database === "sqlite" && options.sqlite === "better-sqlite3") {
+                if (options.sqlite === "better-sqlite3") {
                     imports.addDefault(ast, "better-sqlite3", "Database");
                     imports.addNamed(ast, "drizzle-orm/better-sqlite3", { drizzle: "drizzle" });
 
                     clientExpression = common.expressionFromString("new Database(env.DATABASE_URL)");
                 }
-                if (options.database === "sqlite" && options.sqlite === "turso") {
+                if (options.sqlite === "turso") {
                     imports.addNamed(ast, "@libsql/client", { createClient: "createClient" });
                     imports.addNamed(ast, "drizzle-orm/libsql", { drizzle: "drizzle" });
 
@@ -273,33 +271,33 @@ export const adder = defineAdderConfig({
                     );
                 }
                 // MySQL
-                if (options.database === "mysql" && options.mysql === "mysql2") {
+                if (options.mysql === "mysql2") {
                     imports.addDefault(ast, "mysql2/promise", "mysql");
                     imports.addNamed(ast, "drizzle-orm/mysql2", { drizzle: "drizzle" });
 
                     clientExpression = common.expressionFromString("await mysql.createConnection(env.DATABASE_URL)");
                 }
-                if (options.database === "mysql" && options.mysql === "planetscale") {
+                if (options.mysql === "planetscale") {
                     imports.addNamed(ast, "@planetscale/database", { Client: "Client" });
                     imports.addNamed(ast, "drizzle-orm/planetscale-serverless", { drizzle: "drizzle" });
 
                     clientExpression = common.expressionFromString("new Client({ url: env.DATABASE_URL })");
                 }
                 // PostgreSQL
-                if (options.database === "postgresql" && options.postgresql === "node-postgres") {
+                if (options.postgresql === "node-postgres") {
                     imports.addNamed(ast, "pg", { Client: "Client" });
                     imports.addNamed(ast, "drizzle-orm/node-postgres", { drizzle: "drizzle" });
 
                     clientExpression = common.expressionFromString("new Client(env.DATABASE_URL)");
                 }
-                if (options.database === "postgresql" && options.postgresql === "neon") {
+                if (options.postgresql === "neon") {
                     imports.addNamed(ast, "@neondatabase/serverless", { neon: "neon" });
                     imports.addNamed(ast, "drizzle-orm/neon-http", { drizzle: "drizzle" });
 
                     // TODO: deal with type assertion
                     clientExpression = common.expressionFromString("neon(env.DATABASE_URL!)");
                 }
-                if (options.database === "postgresql" && options.postgresql === "supabase") {
+                if (options.postgresql === "supabase") {
                     imports.addDefault(ast, "postgres", "postgres");
                     imports.addNamed(ast, "drizzle-orm/postgres-js", { drizzle: "drizzle" });
 
@@ -314,7 +312,7 @@ export const adder = defineAdderConfig({
                 common.addStatement(ast, clientIdentifier);
 
                 // Statements after `client` declaration
-                if (options.database === "postgresql" && options.postgresql === "node-postgres") {
+                if (options.postgresql === "node-postgres") {
                     const connectStatement = common.statementFromString("await client.connect();");
                     common.addStatement(ast, connectStatement);
                 }
@@ -326,3 +324,16 @@ export const adder = defineAdderConfig({
         },
     ],
 });
+
+/**
+ * @param {string} content
+ * @param {string} key
+ * @param {string} value
+ * @returns {string}
+ */
+function addEnvVar(content, key, value) {
+    if (!content.includes(key + "=")) {
+        content += `\n${key}=${value}`;
+    }
+    return content;
+}
