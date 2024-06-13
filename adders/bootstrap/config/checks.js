6d28cf77087a671523ac54ea97d4b00d36f7fa33
Manuel Serret <mserret99@gmail.com>
2024-06-13 16:51:47 +0200

    remove precondition test

--- adders/bootstrap/config/checks.js
+++ adders/bootstrap/config/checks.js
@@ -3,16 +3,4 @@ import { options } from "./options";
 
 export const checks = defineAdderChecks({
     options,
-    postconditions: [
-        {
-            name: "scss setup",
-            run: async ({ fileExists, fileContains }) => {
-                if (options.useSass) {
-                    const filePath = `src/variables.scss`;
-                    await fileExists(filePath);
-                    await fileContains(filePath, "$background: lightgrey;");
-                }
-            },
-        },
-    ],
 });
