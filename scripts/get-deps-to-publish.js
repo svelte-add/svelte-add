2f33b3e15828dafa568ec71b93775ec16346acce
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-14 15:07:25 -0400

    small tweak

--- scripts/get-deps-to-publish.js
+++ scripts/get-deps-to-publish.js
@@ -1,20 +1,23 @@
 // @ts-check
 import { execSync } from "node:child_process";
+import { relative } from "node:path";
 
 if (!process.env.CHANGED_DIRS) throw new Error("CHANGED_DIRS is missing");
 
 const dirs = process.env.CHANGED_DIRS.split(" ");
-
 const json = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
-const depsMap = /** @type {import("../packages/core/utils/common.ts").Package[]} */ (JSON.parse(json));
+const depsMap = /** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string }>} */ (JSON.parse(json));
 const packagesToPublish = new Set();
 
 for (const dir of dirs) {
-    // @ts-expect-error `path` exists, just not on the type
     const pkg = depsMap.find((pkg) => pkg.path.endsWith(dir));
+    if (!pkg) throw new Error("couldn't find package in dependency map");
+
     const dependents = depsMap.filter((dep) => dep.dependencies?.[pkg.name] || dep.devDependencies?.[pkg.name]);
-    // @ts-expect-error `path` exists, just not on the type
-    dependents.forEach((dep) => packagesToPublish.add(dep.path));
+    dependents.forEach((dep) => {
+        const relativePath = relative(".", dep.path);
+        packagesToPublish.add(relativePath);
+    });
 }
 
 const toPublish = Array.from(packagesToPublish).join(" ");
