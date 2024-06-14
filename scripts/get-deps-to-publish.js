781371f33ecb08aac0297a30fe91e1ab7ee6fe6b
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-14 15:34:31 -0400

    a little better

--- scripts/get-deps-to-publish.js
+++ scripts/get-deps-to-publish.js
@@ -4,38 +4,36 @@ import { relative } from "node:path";
 
 if (!process.env.CHANGED_DIRS) throw new Error("CHANGED_DIRS is missing");
 
-const dirs = process.env.CHANGED_DIRS.split(" ");
-const json = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
-/** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string }>} */
-const depsMap = JSON.parse(json);
-const packagesToPublish = /** @type {Set<string>} */ (new Set());
+const depsMapJson = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
+const depsMap = /** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string }>} */ (
+    JSON.parse(depsMapJson)
+);
 
-for (const dir of dirs) {
-    getDependents(dir);
-}
+const dirs = process.env.CHANGED_DIRS.split(" ");
+const packagesToPublish = /** @type {Set<string>} */ (new Set(dirs));
 
 // keep looping until we've acquired all dependents
 let prev = packagesToPublish.size;
 while (packagesToPublish.size === prev) {
-    // @ts-expect-error silly error
     for (const pkg of packagesToPublish) {
-        getDependents(pkg);
+        const dependents = addDependents(pkg);
+        dependents.forEach((dep) => packagesToPublish.add(dep));
     }
 }
 
+// publishes packages to pkg-pr-new
 const toPublish = Array.from(packagesToPublish).join(" ");
 execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${toPublish}`, { stdio: "inherit" });
 
 /**
+ * Finds all dependents and their relative paths.
  * @param {string} path
+ * @return {string[]}
  */
-function getDependents(path) {
+function addDependents(path) {
     const pkg = depsMap.find((pkg) => pkg.path.endsWith(path));
     if (!pkg) throw new Error("couldn't find package in dependency map");
 
     const dependents = depsMap.filter((dep) => dep.dependencies?.[pkg.name] || dep.devDependencies?.[pkg.name]);
-    dependents.forEach((dep) => {
-        const relativePath = relative(".", dep.path);
-        packagesToPublish.add(relativePath);
-    });
+    return dependents.map((dep) => relative(".", dep.path));
 }
