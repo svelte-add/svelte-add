f77ff394bedac0506ea293a141b0fad5f94db547
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-14 19:30:59 -0400

    simplify

--- scripts/get-deps-to-publish.js
+++ scripts/get-deps-to-publish.js
@@ -4,13 +4,14 @@ import { relative } from "node:path";
 
 if (!process.env.CHANGED_DIRS) throw new Error("CHANGED_DIRS is missing");
 
-const depsMapJson = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
-const depsMap = /** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string, private: boolean }>} */ (
-    JSON.parse(depsMapJson)
-);
+const json = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
+const repoPackages =
+    /** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string, private: boolean }>} */ (
+        JSON.parse(json)
+    );
 
-const dirs = process.env.CHANGED_DIRS.split(" ");
-const packagesToPublish = /** @type {Set<string>} */ (new Set(dirs));
+const modifiedDirs = process.env.CHANGED_DIRS.split(" ");
+const packagesToPublish = new Set(modifiedDirs);
 
 // keep looping until we've acquired all dependents
 let prev = 0;
@@ -23,8 +24,8 @@ while (packagesToPublish.size !== prev) {
 }
 
 // publishes packages to pkg-pr-new
-const toPublish = Array.from(packagesToPublish).join(" ");
-execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${toPublish}`, { stdio: "inherit" });
+const paths = Array.from(packagesToPublish).join(" ");
+execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${paths}`, { stdio: "inherit" });
 
 /**
  * Finds all dependents and returns their relative paths.
@@ -32,9 +33,11 @@ execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${toPublish}`, { stdio: "inheri
  * @return {string[]}
  */
 function getDependents(path) {
-    const pkg = depsMap.find((pkg) => pkg.path.endsWith(path));
+    const pkg = repoPackages.find((pkg) => pkg.path.endsWith(path));
     if (!pkg) throw new Error("couldn't find package in dependency map");
 
-    const dependents = depsMap.filter((dep) => !dep.private && (dep.dependencies?.[pkg.name] || dep.devDependencies?.[pkg.name]));
+    const dependents = repoPackages.filter(
+        (dep) => !dep.private && (dep.dependencies?.[pkg.name] || dep.devDependencies?.[pkg.name]),
+    );
     return dependents.map((dep) => relative(".", dep.path));
 }
