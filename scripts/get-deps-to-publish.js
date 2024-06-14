bec3815af97d55847c6a3ad256f4b8029dccc7e2
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-14 15:28:03 -0400

    this is so bad but it works

--- scripts/get-deps-to-publish.js
+++ scripts/get-deps-to-publish.js
@@ -8,10 +8,29 @@ const dirs = process.env.CHANGED_DIRS.split(" ");
 const json = execSync(`pnpm -r list --only-projects --json`).toString("utf8");
 /** @type {Array<import("../packages/core/utils/common.ts").Package & { path: string }>} */
 const depsMap = JSON.parse(json);
-const packagesToPublish = new Set();
+const packagesToPublish = /** @type {Set<string>} */ (new Set());
 
 for (const dir of dirs) {
-    const pkg = depsMap.find((pkg) => pkg.path.endsWith(dir));
+    getDependents(dir);
+}
+
+// keep looping until we've acquired all dependents
+let prev = packagesToPublish.size;
+while (packagesToPublish.size === prev) {
+    // @ts-expect-error silly error
+    for (const pkg of packagesToPublish) {
+        getDependents(pkg);
+    }
+}
+
+const toPublish = Array.from(packagesToPublish).join(" ");
+execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${toPublish}`, { stdio: "inherit" });
+
+/**
+ * @param {string} path
+ */
+function getDependents(path) {
+    const pkg = depsMap.find((pkg) => pkg.path.endsWith(path));
     if (!pkg) throw new Error("couldn't find package in dependency map");
 
     const dependents = depsMap.filter((dep) => dep.dependencies?.[pkg.name] || dep.devDependencies?.[pkg.name]);
@@ -20,6 +39,3 @@ for (const dir of dirs) {
         packagesToPublish.add(relativePath);
     });
 }
-
-const toPublish = Array.from(packagesToPublish).join(" ");
-execSync(`pnpm dlx pkg-pr-new@0.0 publish --pnpm ${toPublish}`, { stdio: "inherit" });
