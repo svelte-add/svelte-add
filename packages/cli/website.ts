bd796d39d9fa20bfa23f1eeb89f50c2c40def943
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-17 18:58:34 -0400

    update and simplify

--- packages/cli/website.ts
+++ packages/cli/website.ts
@@ -9,17 +9,11 @@ export function getAdderList(): string[] {
 }
 
 export async function getAdderConfig(name: string) {
-    const config = await executeAdder(name);
-
-    return config;
-}
-
-export async function executeAdder(name: string): Promise<AdderConfig<Record<string, Question>>> {
     // Either vite / rollup or esbuild are not able to process the shebangs
     // present on the `index.js` file. That's why we directly import the configuration
     // for the website here, as this is the only important part.
 
-    const adder: Promise<{ adder: AdderConfig<Record<string, Question>> }> = await import(`../../adders/${name}/config/adder.js`);
+    const adder: Promise<{ adder: AdderConfig<Record<string, Question>> }> = await import(`../../adders/${name}/config/adder.ts`);
     const { adder: adderConfig } = await adder;
 
     return adderConfig;
