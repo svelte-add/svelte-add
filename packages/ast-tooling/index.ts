2f9a963fa826864c8207582b2f8fc3b83a893c44
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-07-01 20:43:22 -0400

    preserve indent format

--- packages/ast-tooling/index.ts
+++ packages/ast-tooling/index.ts
@@ -176,6 +176,39 @@ export function parseJson(content: string) {
 export function serializeJson(originalInput: string, data: unknown) {
     // some of the files we need to process contain comments. The default
     // node JSON.parse fails parsing those comments.
-    // return fleece.patch(originalInput, data);
-    return fleece.stringify(data);
+    const spaces = guessIndentString(originalInput);
+    return fleece.stringify(data, { spaces });
+}
+
+// Sourced from `golden-fleece`
+// https://github.com/Rich-Harris/golden-fleece/blob/f2446f331640f325e13609ed99b74b6a45e755c2/src/patch.ts#L302
+function guessIndentString(str: string): number | undefined {
+    const lines = str.split("\n");
+
+    let tabs = 0;
+    let spaces = 0;
+    let minSpaces = 8;
+
+    lines.forEach((line) => {
+        const match = /^(?: +|\t+)/.exec(line);
+        if (!match) return;
+
+        const whitespace = match[0];
+        if (whitespace.length === line.length) return;
+
+        if (whitespace[0] === "\t") {
+            tabs += 1;
+        } else {
+            spaces += 1;
+            if (whitespace.length > 1 && whitespace.length < minSpaces) {
+                minSpaces = whitespace.length;
+            }
+        }
+    });
+
+    if (spaces > tabs) {
+        let result = "";
+        while (minSpaces--) result += " ";
+        return result.length;
+    }
 }
