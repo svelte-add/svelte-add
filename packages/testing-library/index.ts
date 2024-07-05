0bd41a87989daa52e7123ee0cca46de1928d7631
Manuel Serret <mserret99@gmail.com>
2024-07-05 18:07:41 +0200

    chore: switch to vitest

--- packages/testing-library/index.ts
+++ packages/testing-library/index.ts
@@ -1,9 +1,5 @@
-import { rm } from "node:fs/promises";
-import { generateTestCases, runTestCases } from "./utils/test-cases";
-import { getTemplatesDirectory } from "./utils/workspace";
-import { downloadProjectTemplates } from "./utils/create-project";
-import { remoteControl } from "@svelte-add/core/internal";
-import type { AdderWithoutExplicitArgs } from "@svelte-add/core/adder/config";
+import { generateTestCases, runAdderTests } from "./utils/test-cases";
+import { prepareTests } from "./utils/workspace";
 
 export type TestOptions = {
     headless: boolean;
@@ -11,31 +7,4 @@ export type TestOptions = {
     outputDirectory: string;
 };
 
-export async function testAdder(adder: AdderWithoutExplicitArgs, options: TestOptions) {
-    await testAdders([adder], options);
-}
-
-export async function testAdders(adders: AdderWithoutExplicitArgs[], options: TestOptions) {
-    await prepareTests(options);
-
-    remoteControl.enable();
-    await executeTests(adders, options);
-    remoteControl.disable();
-}
-
-export async function executeTests(adders: AdderWithoutExplicitArgs[], options: TestOptions) {
-    console.log("generating test cases");
-    const testCases = generateTestCases(adders);
-
-    console.log("start testing");
-    await runTestCases(testCases, options);
-}
-
-async function prepareTests(options: TestOptions) {
-    console.log("deleting old files");
-    await rm(options.outputDirectory, { recursive: true, force: true });
-
-    console.log("downloading project templates");
-    const templatesOutputDirectory = getTemplatesDirectory(options);
-    await downloadProjectTemplates(templatesOutputDirectory);
-}
+export { generateTestCases, runAdderTests, prepareTests };
