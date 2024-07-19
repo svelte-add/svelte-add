0b78bb978e0626033fb6f10e2461e44394ec6b85
Aidan Bleser <aidanbleser35@gmail.com>
2024-07-19 15:13:16 -0500

    Need to fix package for correct project name

--- add.ts
+++ add.ts
@@ -1,5 +1,14 @@
-import { create } from "template-factory";
-import * as util from "template-factory/util/index";
+import { create, type Template } from "template-factory";
+import * as util from "template-factory/util";
+import fs from "node:fs";
+import path from "path";
+
+type State = {
+    svelte: boolean;
+    kit: boolean;
+    description: string;
+    docs: string;
+};
 
 const main = async () => {
     await create({
@@ -11,7 +20,101 @@ const main = async () => {
                 path: util.relative("./adders/template", import.meta.url),
                 flag: "custom-adder",
                 excludeFiles: ["README.md"],
-            },
+                state: { svelte: false, kit: false, description: "", docs: "" },
+                prompts: [
+                    {
+                        kind: "text",
+                        message: "Description of the adder",
+                        validate: (value) => {
+                            if (value == "") return "Please enter a description for your adder";
+                        },
+                        result: {
+                            run: async (result, { state }) => {
+                                state.description = result;
+                            },
+                        },
+                    },
+                    {
+                        kind: "text",
+                        message: "Link to documentation for the adder",
+                        validate: (value) => {
+                            if (value == "") return "Please include a documentation link for your adder";
+
+                            try {
+                                new URL(value);
+                            } catch (_) {
+                                return "Please enter a valid URL";
+                            }
+                        },
+                        result: {
+                            run: async (result, { state }) => {
+                                state.docs = result;
+                            },
+                        },
+                    },
+                    {
+                        kind: "confirm",
+                        message: "Is this an adder for Svelte?",
+                        initialValue: true,
+                        yes: {
+                            run: async ({ state }) => {
+                                state.svelte = true;
+
+                                return [
+                                    {
+                                        kind: "confirm",
+                                        message: "Is this an adder for SvelteKit?",
+                                        yes: {
+                                            run: async ({ state }) => {
+                                                state.kit = true;
+                                            },
+                                        },
+                                    },
+                                ];
+                            },
+                        },
+                    },
+                    {
+                        kind: "select",
+                        message: "What type of integration is this?",
+                        initialValue: "Inline",
+                        options: [
+                            {
+                                name: "External",
+                            },
+                            {
+                                name: "Inline",
+                            },
+                        ],
+                        result: {
+                            run: async (result, { state, projectName, dir }) => {
+                                const adderPath = path.join(dir, "config/adder.ts");
+
+                                let adderTsContent = fs.readFileSync(adderPath).toString();
+
+                                adderTsContent = adderTsContent.replace("ID", projectName);
+                                adderTsContent = adderTsContent.replace("NAME", projectName);
+                                adderTsContent = adderTsContent.replace("DESCRIPTION", state.description);
+                                adderTsContent = adderTsContent.replace("DOCUMENTATION_LINK", state.docs);
+                                adderTsContent = adderTsContent.replace("INTEGRATION_TYPE", result);
+                                adderTsContent = adderTsContent.replace("YOUR_SVG_HERE", projectName);
+                                adderTsContent = adderTsContent.replace("SVELTE", state.svelte.toString());
+                                adderTsContent = adderTsContent.replace("KIT", state.kit.toString());
+
+                                console.log("pre rename");
+
+                                fs.renameSync(path.join(dir, "YOUR_SVG_HERE.svg"), path.join(dir, `${projectName}.svg`));
+
+                                console.log("rename");
+
+                                fs.writeFileSync(adderPath, adderTsContent);
+
+                                console.log("wrote");
+                            },
+                        },
+                    },
+                ],
+            } satisfies Template<State>,
         ],
     });
 };
