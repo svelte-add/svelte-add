c25b030814b81eb0114b19d07105081038f52a3a
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-07-24 19:28:51 -0400

    silly rule

--- eslint.config.js
+++ eslint.config.js
@@ -32,6 +32,7 @@ export default tseslint.config(
             "@typescript-eslint/no-unsafe-call": "off",
             "@typescript-eslint/no-unsafe-member-access": "off",
             "@typescript-eslint/no-unsafe-assignment": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
         },
     },
     {
