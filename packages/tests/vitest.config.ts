60c3e6161ce48692f6233344f5159d3e0693e65d
Manuel Serret <mserret99@gmail.com>
2024-07-28 11:53:05 +0200

    format

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: {
-        include: ["./*.ts"],
-        testTimeout: 1000 * 60 * 2, // 2 minutes
-        hookTimeout: 1000 * 60, // 1 minute
-        pool: "threads",
-    },
+	test: {
+		include: ['./*.ts'],
+		testTimeout: 1000 * 60 * 2, // 2 minutes
+		hookTimeout: 1000 * 60, // 1 minute
+		pool: 'threads',
+	},
 });
