c8e61ebec94ce5ad6e8e763485c7afd584848110
LorisSigrist <loris@sigrist.dev>
2024-08-02 15:17:28 +0200

    Add link to original Regex

--- adders/paraglide/config/options.ts
+++ adders/paraglide/config/options.ts
@@ -20,6 +20,7 @@ export const options = defineAdderOptions({
 });
 
 export const isValidLanguageTag = (languageTag: string): boolean =>
+	// Regex vendored in from https://github.com/opral/monorepo/blob/94c2298cc1da5378b908e4c160b0fa71a45caadb/inlang/source-code/versioned-interfaces/language-tag/src/interface.ts#L16
 	RegExp(
 		'^((?<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?<language>([A-Za-z]{2,3}(-(?<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?))(-(?<script>[A-Za-z]{4}))?(-(?<region>[A-Za-z]{2}|[0-9]{3}))?(-(?<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*))$',
 	).test(languageTag);
