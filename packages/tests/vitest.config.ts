bd73f5a174c66e1efbebc66d9228504acc753f4b
Manuel Serret <mserret99@gmail.com>
2024-08-13 21:10:22 +0200

    move integration tests

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['./*.ts'],
+		include: ['./integration/*.ts', './unit/*.ts'],
 		testTimeout: 1000 * 60 * 2, // 2 minutes
 		hookTimeout: 1000 * 60 * 3, // 3 minutes
 		pool: 'threads',
